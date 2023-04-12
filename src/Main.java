9f1e8870d8c5de951febbf5f1d203892f287a59d
sarrust <103333432+sarrust@users.noreply.github.com>
2023-04-12 17:54:47 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        canner in = new Scanner(System.in);
+        String[] robotText = {"Камень", "Ножницы", "Бумага"};
 
+        System.out.println("Это игра Камень-Ножницы-Бумага!\nВыбирайте одно или для выхода нажмите \"Выход\": ");
+        while(true) {
+            String text = in.nextLine();
+            if(text.equals("Выход")) {
+                System.out.println("Спасибо за игру! Всего доброго!");
+                break;
+            }
+            int index = new Random().nextInt(robotText.length);
+            if(!text.equals("Камень") && !text.equals("Ножницы") && !text.equals("Бумага")) {
+                System.out.println("Некорректный ввод! Повторите попытку: ");
+            } else {
+                if ((text.equals("Бумага") && robotText[index].equals("Камень")) ||
+                        (text.equals("Камень") && robotText[index].equals("Ножницы")) ||
+                        (text.equals("Ножницы") && robotText[index].equals("Бумага"))) {
+                    System.out.println("Компьютер: \n" + robotText[index]);
+                    System.out.println("Вы победили");
+                }
+                if (text.equals(robotText[index])) {
+                    System.out.println("Компьютер: \n" + robotText[index]);
+                    System.out.println("Ничья");
+                }
+                if ((text.equals("Камень") && robotText[index].equals("Бумага")) ||
+                        (text.equals("Ножницы") && robotText[index].equals("Камень")) ||
+                        (text.equals("Бумага") && robotText[index].equals("Ножницы"))) {
+                    System.out.println("Компьютер: \n" + robotText[index]);
+                    System.out.println("Компьютер победил");
+                }
+            }
+        }
     }
 }
